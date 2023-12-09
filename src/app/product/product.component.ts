4e0077170803c63aecaf757ae9792e9dad48dc52
othmanetozy <tozyothmane123@gmail.com>
2023-12-09 22:26:27 +0100

    🤦 fix Bug search Products

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,47 +8,44 @@ import {Router} from "@angular/router";
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit{
-  products! : Array<Product>;
-  keyword: any = '';
-  totalPages: number=3;
-  PageSize: number=2;
-  currentPage: number=2;
-  constructor(private productService:ProductService) {
+export class ProductComponent implements OnInit {
+  products!: Array<Product>;
+  keyword: any = "";
+  totalPages: number = 3;
+  PageSize: number = 2;
+  currentPage: number = 1; // Corrected the initial page to 1
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.searchProduct();
   }
-  ngOnInit(): void {
-   this.searchProduct();
-  }
-  getProduct(){
-   this.productService.getProduct(this.keyword,this.currentPage,this.PageSize)
+
+  searchProduct() {
+    this.productService.searchProduct(this.keyword, this.currentPage, this.PageSize)
       .subscribe({
-          next: (resp) =>
-          {
-            this.products=resp.body as Product[];
-            let totalProducts: number = parseInt(resp.headers.get('x-total-count')!);
-            this.totalPages = Math.floor(totalProducts/this.PageSize);
-            if (totalProducts % this.PageSize != 0)
-            {
-              this.totalPages = this.totalPages+1;
-            }
-          },
-          error : err => {
-            console.log(err)
+        next: (resp) => {
+          this.products = resp.body as Product[];
+          let totalProducts: number = parseInt(resp.headers.get('x-total-count')!);
+          this.totalPages = Math.floor(totalProducts / this.PageSize);
+          if (totalProducts % this.PageSize !== 0) {
+            this.totalPages = this.totalPages + 1;
           }
+        },
+        error: err => {
+          console.log(err);
         }
-      )
+      });
   }
 
   handleCheckProduct(product: Product) {
     this.productService.checkProduct(product).subscribe({
-        next : updateProduct => {
-          product.checked =!product.checked;
-          //le seul produit qon a cheque
-          }
-         })
-        }
-
+      next: updateProduct => {
+        product.checked = !product.checked;
+        // the only product that has been checked
+      }
+    });
+  }
 
   handleDelete(product: Product) {
     if (confirm("Êtes-vous sûr de vouloir supprimer?")) {
@@ -60,19 +57,8 @@ export class ProductComponent implements OnInit{
     }
   }
 
- /* searchProduct() {
-    this.currentPage=1;
-    this.totalPages = 3;
-    this.productService.searchProduct(this.keyword , this.PageSize , this.currentPage).subscribe({
-      next : value => {
-        this.products=value;
-      }
-    });
-  }*/
-
-  handleGoToPage(page : number){
-      this.currentPage=page;
-      this.getProduct();
+  handleGoToPage(page: number) {
+    this.currentPage = page;
+    this.searchProduct();
   }
 }
-
